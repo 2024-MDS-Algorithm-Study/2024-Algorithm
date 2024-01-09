e42e7c9bbe443adfc62cf71e16673ba1db4b371b
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-01-09 13:17:16 +0900

    feat: BOJ11051 이항 계수 2

---
+++ minjeong/BOJ/src/BOJ11051.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int k = Integer.parseInt(s[1]);
+
+        int[][] dp = new int[n + 1][n + 1];
+        dp[0][0] = 1;
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+
+        System.out.println(dfs(n, k, dp));
+    }
+
+    private static int dfs(int n, int k, int[][] dp) {
+        if (n < k) return 0;
+        if (dp[n][k] != 0) return dp[n][k];
+        if (k == 0) return dp[n][k] = 1;
+        return dp[n][k] = (dfs(n - 1, k - 1, dp) + dfs(n - 1, k, dp)) % 10007;
+    }
+}
