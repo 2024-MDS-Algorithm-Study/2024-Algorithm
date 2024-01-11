9c725796d253b9a3372736f6aca3bdd4a7095206
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-01-12 01:21:38 +0900

    feat: BOJ20056 마법사 상어와 파이어볼

---
+++ minjeong/BOJ/src/BOJ20056.java
@@ -0,0 +1,128 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] del = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
+    static List<Node>[][] balls;
+    static int N, M, K, res;
+    static class Node {
+        int m;
+        int s;
+        int d;
+
+        Node(int m, int s, int d) {
+            this.m = m;
+            this.s = s;
+            this.d = d;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        makeInputs();
+
+        for (int i = 0; i < K; i++) {
+            List<Node>[][] tmpBalls = setList();
+            stepOne(N, tmpBalls);
+            stepTwo(N, tmpBalls);
+            balls = tmpBalls;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j =1; j <= N; j++) {
+                for (Node ball : balls[i][j]) {
+                    res += ball.m;
+                }
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    private static void stepOne(int N, List<Node>[][] tmpBalls) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (balls[i][j].isEmpty()) continue;
+                move(i, j, tmpBalls);
+            }
+        }
+    }
+
+    private static void move(int x, int y, List<Node>[][] tmpBalls) {
+        for (Node ball : balls[x][y]) {
+            int nx = x;
+            int ny = y;
+            for (int i = 0; i < ball.s; i++) {
+                nx += del[ball.d][0];
+                if (nx == N + 1) nx = 1;
+                if (nx == 0) nx = N;
+                ny += del[ball.d][1];
+                if (ny == N + 1) ny = 1;
+                if (ny == 0) ny = N;
+            }
+
+            tmpBalls[nx][ny].add(new Node(ball.m, ball.s, ball.d));
+        }
+    }
+
+    private static void stepTwo(int N, List<Node>[][] tmpBalls) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (tmpBalls[i][j].size() < 2) continue;
+                split(i, j, tmpBalls);
+            }
+        }
+    }
+
+    private static void split(int x, int y, List<Node>[][] tmpBalls) {
+        int mSum = 0, sSum = 0, odd = 0;
+        for (Node tmpBall : tmpBalls[x][y]) {
+            mSum += tmpBall.m;
+            sSum += tmpBall.s;
+            if (tmpBall.d % 2 == 1) odd++;
+        }
+
+        int newM = mSum / 5;
+        int newS = sSum / tmpBalls[x][y].size();
+        int size = tmpBalls[x][y].size();
+        tmpBalls[x][y].clear();
+        if (newM == 0) return;
+        if (odd == 0 || odd == size)
+            for (int i = 0; i < 8; i += 2)
+                tmpBalls[x][y].add(new Node(newM, newS, i));
+        else
+            for (int i = 1; i < 8; i += 2)
+                tmpBalls[x][y].add(new Node(newM, newS, i));
+    }
+
+    private static void makeInputs() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        balls = setList();
+        for (int i = 0; i < M; i++) {
+            String[] inputs = br.readLine().split(" ");
+            int a, b;
+            a = Integer.parseInt(inputs[0]); b = Integer.parseInt(inputs[1]);
+            balls[a][b].add(new Node(
+                    Integer.parseInt(inputs[2]),
+                    Integer.parseInt(inputs[3]),
+                    Integer.parseInt(inputs[4])
+            ));
+        }
+    }
+
+    private static List<Node>[][] setList() {
+        List<Node>[][] list = new List[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                list[i][j] = new ArrayList<>();
+            }
+        }
+        return list;
+    }
+}
