dd640231bc9adc8d06dbd4ebbe75e3f78c395e09
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-01-15 20:54:02 +0900

    feat: BOJ1904 01타일

---
+++ minjeong/BOJ/src/BOJ1904.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1; dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
