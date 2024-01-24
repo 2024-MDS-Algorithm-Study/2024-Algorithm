c8f5673698e54cf68e6a52fbcc4716e36164415a
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-01-24 16:57:23 +0900

    feat: BOJ15565 귀여운 라이언

---
+++ minjeong/BOJ/src/BOJ15565.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        int res = Integer.MAX_VALUE;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n, k;
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int start = 0, end = 0, cnt = 0;
+        if (arr[end] == 1) cnt++;
+        while (start <= end && start < n && end < n) {
+            if (cnt < k) {
+                end++;
+                if (end < n && arr[end] == 1) cnt++;
+            }
+            else {
+                if (cnt == k) res = Math.min(res, end - start + 1);
+                if (arr[start] == 1) cnt--;
+                start++;
+            }
+        }
+
+        if (res == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(res);
+    }
+}
