39e03841a382d7138d747cbb3f5b2fd2d79b1479
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-02 17:24:15 +0900

    feat: BOJ1655 가운데를 말해요

---
+++ minjeong/BOJ/src/BOJ1655.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
+
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            int a = Integer.parseInt(br.readLine());
+            if (pq.size() == pq2.size()) pq.offer(-a);
+            else pq2.offer(a);
+
+            if (!pq2.isEmpty() && (-pq.peek() > pq2.peek())) {
+                int tmp = -pq.poll();
+                pq.offer(-pq2.poll());
+                pq2.offer(tmp);
+            }
+
+            sb.append(-pq.peek()).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
