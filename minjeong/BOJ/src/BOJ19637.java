83932007b497badf5c2390452e980367585dedbc
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-04 15:29:07 +0900

    feat: BOJ19637 IF문 좀 대신 써줘

---
+++ minjeong/BOJ/src/BOJ19637.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        TreeMap<Integer, String> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int power = Integer.parseInt(st.nextToken());
+            if (map.containsKey(power)) continue;
+            map.put(power, name);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            sb.append(map.ceilingEntry(Integer.parseInt(br.readLine())).getValue()).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
