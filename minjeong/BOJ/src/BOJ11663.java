43c1e6ddf59ee7f5e291e25fc955deb65744fffb
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-14 12:06:03 +0900

    feat: BOJ11663 선분 위의 점

---
+++ minjeong/BOJ/src/BOJ11663.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n, m, points[];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        points = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            points[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(points);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s, e;
+            s = Integer.parseInt(st.nextToken());
+            e = Integer.parseInt(st.nextToken());
+            int sIdx = Arrays.binarySearch(points, s);
+            int eIdx = Arrays.binarySearch(points, e);
+            if (sIdx < 0) sIdx = -(sIdx + 1);
+            if (eIdx < 0) eIdx = -(eIdx + 1);
+            else eIdx++;
+            sb.append(eIdx - sIdx).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
