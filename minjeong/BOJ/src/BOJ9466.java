505b76fe2bd1b2a05fcfa9ea9256bfee3f6cf379
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-14 10:55:40 +0900

    feat: BOJ9466 텀 프로젝트

---
+++ minjeong/BOJ/src/BOJ9466.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, res;
+    static int[] students = new int[100001];
+    static boolean[] v = new boolean[100001], made;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (t-- > 0) {
+            n = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            res = n;
+            for (int i = 0; i < n; i++) {
+                students[i] = Integer.parseInt(st.nextToken()) - 1;
+            }
+
+            made = new boolean[n];
+            for (int i = 0; i < n; i++) {
+                if (!made[i]) solve(i);
+            }
+            sb.append(res).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    private static void solve(int pos) {
+        if (v[pos]) {
+            int next = pos;
+            do {
+                made[next] = true;
+                next = students[next];
+                res--;
+            } while (next != pos);
+            return;
+        }
+
+        v[pos] = true;
+        if (!made[students[pos]]) solve(students[pos]);
+        v[pos] = false;
+        made[pos] = true;
+    }
+}
