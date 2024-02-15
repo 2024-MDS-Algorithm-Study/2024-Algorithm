d57f77d979eee71c75b4305cfa6fba3f5f39e6af
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-15 20:15:37 +0900

    feat: BOJ2239 스도쿠

--- minjeong/BOJ/src/BOJ2239.java
+++ minjeong/BOJ/src/BOJ2239.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class Main {
     static int[][] board = new int[9][9];
-    static boolean flag;
     static List<Node> list = new ArrayList<>();
     static class Node {
         int x;
@@ -27,48 +26,39 @@ public class Main {
                 if (board[i][j] == 0) list.add(new Node(i, j));
             }
         }
-        
+
         solve(0);
-    }
 
-    private static void solve(int cnt) {
-        if (flag) return;
-        if (cnt == list.size()) {
-            print();
-            flag = true;
-            return;
-        }
-
-        int x = list.get(cnt).x;
-        int y = list.get(cnt).y;
-        for (int num = 1; num <= 9; num++) {
-            if (!flag && check(x, y, num)) {
-                board[x][y] = num;
-                solve(cnt + 1);
-            }
-        }
-        board[x][y] = 0;
-    }
-
-    private static boolean check(int x, int y, int num) {
-        for (int i = 0; i < 9; i++) {
-            if (board[(x / 3) * 3 + i / 3][(y / 3) * 3 + i % 3] == num) return false;
-        }
-
-        for (int i = 0; i < 9; i++) {
-            if (board[x][i] == num) return false;
-            if (board[i][y] == num) return false;
-        }
-
-        return true;
-    }
-
-    private static void print() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 System.out.print(board[i][j]);
             }
             System.out.println();
         }
+    }
+
+    static boolean solve(int cnt) {
+        if (cnt == list.size()) return true;
+
+        int x = list.get(cnt).x;
+        int y = list.get(cnt).y;
+        for (int num = 1; num <= 9; num++) {
+            if (check(x, y, num)) {
+                board[x][y] = num;
+                if (solve(cnt + 1)) return true;
+            }
+        }
+        board[x][y] = 0;
+        return false;
+    }
+
+    static boolean check(int x, int y, int num) {
+        for (int i = 0; i < 9; i++)
+            if (board[(x / 3) * 3 + i / 3][(y / 3) * 3 + i % 3] == num) return false;
+
+        for (int i = 0; i < 9; i++)
+            if (board[x][i] == num || board[i][y] == num) return false;
+
+        return true;
     }
 }
