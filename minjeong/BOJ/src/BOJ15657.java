e9cecadf6b1361dcf6b99f9b43944a28dddadf53
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-17 15:43:49 +0900

    feat: BOJ15657 N과 M (8)

---
+++ minjeong/BOJ/src/BOJ15657.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, arr[];
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        solve(0, 0, new int[m]);
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve(int pos, int depth, int[] res) throws Exception {
+        if (depth == m) {
+            for (int x : res) bw.write(x + " ");
+            bw.write('\n');
+            return;
+        }
+
+        for (int i = pos; i < n; i++) {
+            res[depth] = arr[i];
+            solve(i, depth + 1, res);
+        }
+    }
+}
