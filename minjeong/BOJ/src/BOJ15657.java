1b023315d7b8522277c27a419886f03861d698a6
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-17 15:51:27 +0900

    feat: BOJ15657 N과 M (8)

--- minjeong/BOJ/src/BOJ15657.java
+++ minjeong/BOJ/src/BOJ15657.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int n, m, arr[];
