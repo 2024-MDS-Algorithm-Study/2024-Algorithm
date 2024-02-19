d7623a9a4729ea29763401a79896d92fa7f4c26f
minpaeng <68097132+minpaeng@users.noreply.github.com>
2024-02-19 10:18:50 +0900

    feat: BOJ11055 가장 큰 증가하는 부분 수열

---
+++ minjeong/BOJ/src/BOJ11055.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = dp[i] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) dp[i] = Math.max(dp[j] + arr[i], dp[i]);
+            }
+            res = Math.max(dp[i], res);
+        }
+
+        System.out.println(res);
+    }
+}
